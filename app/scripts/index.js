5ddd288ec5109ad470ac5e94806d19431a95cb6a
Matt Morris <mcm0027@yahoo.com>
2016-03-04 17:25:19 -0600

    add screenshots to myWork section

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -12,13 +12,34 @@
 
     }])
     .controller('mainController', [
-        mainController]);
+        mainController])
 
-
+    .factory('projectsService', [
+        projectsService
+    ]);
 
     function mainController(){
 
     }
 
 })(window.angular);
+
+var hi = function(){
+    alert("hi");
+};
+
+var hiButton = document.getElementById("button")
+
+    hiButton.onclick = hi;
+
+var poopButton = document.getElementById("poop")
+
+poopButton.onclick = hi();
+
+var showButton = document.getElementById("show");
+
+showButton.onclick = function(){
+    console.log("show " + hiButton);
+    console.log("poop " + poopButton);
+}
 
--- app/scripts/myWork.js
+++ app/scripts/myWork.js
@@ -1,12 +1,15 @@
 (function(angular) {
     'use strict';
-    function MyWorkController() {
-
+    function MyWorkController(projectsService) {
+        var vm = this;
+        vm.projects = projectsService.set();
+        console.log(vm.projects);
     }
 
     angular.module('myApp').component('myWork', {
         templateUrl: './app/views/myWork.html',
         controller: MyWorkController,
+        controllerAs: 'vm',
         bindings: {
             //hero: '='
         }
