144cfe297a3ef6272cde37463254b927e779deca
Matt Morris <mcm0027@yahoo.com>
2016-03-05 13:18:32 -0600

    remove test buttons

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -23,23 +23,3 @@
     }
 
 })(window.angular);
-
-var hi = function(){
-    alert("hi");
-};
-
-var hiButton = document.getElementById("button")
-
-    hiButton.onclick = hi;
-
-var poopButton = document.getElementById("poop")
-
-poopButton.onclick = hi();
-
-var showButton = document.getElementById("show");
-
-showButton.onclick = function(){
-    console.log("show " + hiButton);
-    console.log("poop " + poopButton);
-}
-
