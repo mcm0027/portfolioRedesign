6a1452d979ceb93e26a813901bc516c1f27f5a34
Matt Morris <mcm0027@yahoo.com>
2016-03-10 19:02:07 -0600

    add modal js logic and sample template

--- app/scripts/myWork.js
+++ app/scripts/myWork.js
@@ -1,8 +1,20 @@
 (function(angular) {
     'use strict';
-    function MyWorkController(projectsService) {
+    function MyWorkController(projectsService, $modal) {
         var vm = this;
         vm.projects = projectsService.set();
+        vm.openModal = function($event) {
+            var modalInstance = $modal.open({
+                templateUrl: 'app/views/modals/projectModal.html',
+                anchorElement: $event ? angular.element($event.target) : undefined,
+
+            });
+            modalInstance.result.then(function (selectedItem) {
+                vm.modalResult = 'You selected ' + selectedItem;
+            }, function () {
+                vm.modalResult = 'You dismissed the modal';
+            });
+        };
     }
 
     angular.module('myApp').component('myWork', {
