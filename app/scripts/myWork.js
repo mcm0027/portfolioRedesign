13060ee3bc067eca89ef8f45df79810fbd4ec9a3
Matt Morris <mcm0027@yahoo.com>
2016-03-10 23:03:17 -0600

    add links to portfolio items and remove modal

--- app/scripts/myWork.js
+++ app/scripts/myWork.js
@@ -3,18 +3,7 @@
     function MyWorkController(projectsService, $modal) {
         var vm = this;
         vm.projects = projectsService.set();
-        vm.openModal = function($event) {
-            var modalInstance = $modal.open({
-                templateUrl: 'app/views/modals/projectModal.html',
-                anchorElement: $event ? angular.element($event.target) : undefined,
 
-            });
-            modalInstance.result.then(function (selectedItem) {
-                vm.modalResult = 'You selected ' + selectedItem;
-            }, function () {
-                vm.modalResult = 'You dismissed the modal';
-            });
-        };
     }
 
     angular.module('myApp').component('myWork', {
