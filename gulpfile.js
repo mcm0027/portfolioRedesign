26b7e668c1423db3f725a8b7798ab34b1d2a5053
Matt Morris <mcm0027@yahoo.com>
2016-03-16 12:51:57 -0500

    add sass compiler and begin converting css to sass

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,14 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var cleanCSS = require('gulp-clean-css');
+var sass = require('gulp-sass');
+
+gulp.task('css', function() {
+    return gulp.src('app/styles/*.css')
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest('./dist/styles/'));
+});
 
 gulp.task('js', function () {
     return gulp.src(['app/scripts/projectsService.js', 'app/scripts/index.js', 'app/scripts/*.js'])
@@ -12,5 +20,15 @@ gulp.task('js', function () {
 
 gulp.task('html', function () {
     return gulp.src('app/views/*.html')
-        .pipe(gulp.dest('./dist/views/'))
+    Untitled event        .pipe(gulp.dest('./dist/views/'))
+});
+
+gulp.task('sass', function () {
+    return gulp.src('app/styles/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./dist/styles/'));
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch('/app/styles/**/*.scss', ['sass']);
 });
